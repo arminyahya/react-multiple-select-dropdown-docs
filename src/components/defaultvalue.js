6772aadc407f0e9ddd802b378402892da5656e42
yahya <yahya@DEVELOPER1>
2019-07-14 11:13:11 +0430

    REFACTOR: Change defaultValue component

--- src/components/defaultvalue.js
+++ src/components/defaultvalue.js
@@ -1,11 +1,10 @@
 import * as React from "react"
-import styled from 'styled-components';
+import styled from "styled-components"
 
-
-const DefaultValue = props => <span {...props}>default: {props.children}</span>;
-
-export default styled(DefaultValue)`
-	background-color: rgb(244, 245, 247);
+const DefaultValue = props => <span {...props}>default: {props.children}</span>
+const styledDefaultValue = styled(DefaultValue)`
+  background-color: rgb(244, 245, 247);
   font-size: 15px;
   padding: 4px;
 `
+export default styledDefaultValue
